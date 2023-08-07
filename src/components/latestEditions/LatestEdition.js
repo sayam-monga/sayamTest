39310ec6fded9e5ef70aba4a61093e208cdceedf
sarthaksri <sarthaksri017@gmail.com>
2023-08-07 16:40:55 +0530

    LatestEditiion completed with styling bugs

--- src/components/latestEditions/LatestEdition.js
+++ src/components/latestEditions/LatestEdition.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ascesionImg from '../../Media/Ascesion.jpeg';
-import styles1 from './LatestEdition.module.css'
+import React from "react";
+import ascesionImg from "../../Media/Ascesion.jpeg";
+import styles1 from "./LatestEdition.module.css";
 
 function LatestEdition() {
   return (
     <div className={styles1.container}>
       <img className={styles1.ascesionImg} src={ascesionImg} alt="Ascesion" />
+      <section>
+      <div className={`${styles1.scroll} ${styles1.text}`}>
+       <div>
+        ASCESION ASCESION ASCESION ASCESION
+        </div>
+        <div>
+        ASCESION ASCESION ASCESION 
+        </div>
+      </div>
+      </section>
     </div>
-  )
+  );
 }
 
-export default LatestEdition
+export default LatestEdition;
